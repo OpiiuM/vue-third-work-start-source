0f7e2f73654af057b0c75483d66660e0cd27e945
Николай Мамаенко <dycsollll@gmail.com>
2023-03-15 18:47:39 +0300

    Настройка проекта и базовая разметка

---
+++ demo/src/demo0/DemoCounter.vue
@@ -0,0 +1,47 @@
+<template>
+    <div class="counter">
+        <button @click="decrement">-</button>
+        <span>Current value: {{ count }}</span>
+        <button @click="increment">+</button>
+    </div>
+</template>
+
+<script setup>
+import { ref } from "vue";
+
+const count = ref(0);
+
+const decrement = () => {
+    count.value = count.value - 1;
+};
+
+const increment = () => {
+    count.value = count.value + 1;
+};
+</script>
+
+<style lang="scss" scoped>
+.counter {
+    display: flex;
+    align-items: center;
+
+    button {
+        font-size: 1.5rem;
+        
+        width: 2rem;
+        height: 2rem;
+        margin: 0 10px;
+        
+        cursor: pointer;
+        
+        color: white;
+        border: none;
+        border-radius: 50%;
+        background: #dd0000;
+
+        &:hover {
+            background: #ff0000;
+        }
+    }
+}
+</style>
