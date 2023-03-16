4831269bfaf314a77f34312a312b25fe96ce596c
Николай Мамаенко <dycsollll@gmail.com>
2023-03-16 19:46:55 +0300

    Взаимодействие между компонентами

---
+++ demo/src/demo1/DemoCounter.vue
@@ -0,0 +1,33 @@
+<template>
+    <demo-container>
+        <div class="counter">
+            <counter-button @action="decrement">-</counter-button>
+            <counter-value :value="count" />
+            <counter-button @action="increment">+</counter-button>
+        </div>
+    </demo-container>
+</template>
+
+<script setup>
+import { ref } from 'vue';
+import DemoContainer from '@/components/DemoContainer.vue';
+import CounterButton from '@/demo1/CounterButton.vue';
+import CounterValue from '@/demo1/CounterValue.vue';
+
+const count = ref(0);
+
+const decrement = () => {
+    count.value = count.value - 1;
+};
+
+const increment = () => {
+    count.value = count.value + 1;
+};
+</script>
+
+<style lang="scss" scoped>
+.counter {
+    display: flex;
+    align-items: center;
+}
+</style>
--- frontend/src/layouts/AppLayout.vue
+++ frontend/src/layouts/AppLayout.vue
@@ -1,12 +1,34 @@
 <template>
-    <div class="app_layout">
-        <app-layout-header />
-        <slot />
+  <div class="app_layout">
+    <app-layout-header />
+    <div class="content">
+      <app-layout-sidebar
+        :tasks="props.tasks"
+        :filters="props.filters"
+        @update-tasks="$emit('updateTasks', $event)"
+      />
     </div>
+    <slot />
+  </div>
 </template>
 
 <script setup>
-import AppLayoutHeader from "@/layouts/AppLayoutHeader.vue";
+import { defineProps, defineEmits } from 'vue';
+import AppLayoutHeader from '@/layouts/AppLayoutHeader.vue';
+import AppLayoutSidebar from '@/layouts/AppLayoutSidebar.vue';
+
+const props = defineProps({
+  tasks: {
+    type: Array,
+    required: true,
+  },
+  filters: {
+    type: Object,
+    required: true,
+  },
+});
+
+defineEmits(['updateTasks']);
 </script>
 
 <style lang="scss" scoped>
@@ -15,5 +37,10 @@ import AppLayoutHeader from "@/layouts/AppLayoutHeader.vue";
   flex-direction: column;
 
   height: 100vh;
+}
+
+.content {
+  display: flex;
+  flex-grow: 1;
 }
 </style>
